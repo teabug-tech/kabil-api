8a9da649bc33a7bcab5401130b186419a6a04372
Mohamed taha Baz <mobaz@e1r6p10.1337.ma>
2021-10-29 09:20:51 +0100

    add dialectSchema

--- src/models/Dialect.ts
+++ src/models/Dialect.ts
@@ -1,1 +1,12 @@
 import { model, Schema } from 'mongoose';
+
+const dialectSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const dialectModel = model('dialect', dialectSchema);
+
+export default dialectModel;
