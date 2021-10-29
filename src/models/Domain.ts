c2ac3568629da16d3bfe95914a8439f6fc68f243
Tahabz <etahabaz@gmail.com>
2021-10-29 13:00:08 +0100

    Add IDomain type interface

--- src/models/Domain.ts
+++ src/models/Domain.ts
@@ -1,6 +1,12 @@
-import { model, Schema } from 'mongoose';
+import { model, PopulatedDoc, Schema } from 'mongoose';
+import { IUser } from './User';
 
-const domainSchema = new Schema({
+interface IDomain {
+  name: string;
+  user: PopulatedDoc<IUser>;
+}
+
+const domainSchema = new Schema<IDomain>({
   name: {
     type: String,
     required: true,
@@ -14,4 +20,5 @@ const domainSchema = new Schema({
 
 const domainModel = model('domain', domainSchema);
 
+export { IDomain };
 export default domainModel;
