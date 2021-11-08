8769305718578c6feee3a7534cd3dd5d8feeb5dc
Tahabz <etahabaz@gmail.com>
2021-11-08 09:58:06 +0100

    remove google auth logic from index.ts and export it as a seperate route

--- src/index.ts
+++ src/index.ts
@@ -3,36 +3,14 @@ import morgan from 'morgan';
 import connect from './db/connect';
 import * as dotenv from 'dotenv';
 import errorMiddleware from './middlewares/errorMiddleware';
-import Test from './models/Test';
 import testRouter from './routes/testRoute';
-import { auth } from '@googleapis/docs';
-import { google } from 'googleapis';
 import authMiddleware from './middlewares/authMiddleware';
 import AuthController from './controllers/AuthController';
-import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
+import googleAuthRouter from './routes/googleAuthRoute';
 
 dotenv.config();
 const app = express();
-
-const oauth2Client = new auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  process.env.REDIRECT_URL
-);
-
-const oauth2 = google.oauth2({ auth: oauth2Client, version: 'v2' });
-
-// generate a url that asks permissions for Blogger and Google Calendar scopes
-const scopes = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'];
-
-const url = oauth2Client.generateAuthUrl({
-  // 'online' (default) or 'offline' (gets refresh_token)
-  access_type: 'offline',
-
-  // If you only need one scope you can pass it as a string
-  scope: scopes,
-});
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -49,21 +27,12 @@ const start = async () => {
   }
 };
 
+app.use(googleAuthRouter);
+
 app.use('/test', testRouter);
-app.get('/', (req, res, next) => {
-  return res.send(url);
-});
-
-app.get('/auth/google', async (req, res) => {
-  const { access_token, id_token } = (await oauth2Client.getToken(req.query.code as string)).tokens;
-  oauth2Client.setCredentials({ access_token, id_token });
-  const user = (await oauth2.userinfo.get()).data;
-
-  res.cookie('JWT', jwt.sign(user, process.env.JWT_SECRET));
-  res.redirect('http://localhost:4444/hello');
-});
 
 app.post('/sign', AuthController);
+
 app.use(authMiddleware);
 app.get('/hello', (req, res) => {
   return res.end('hello world');
---
+++ src/routes/googleAuthRoute.ts
@@ -0,0 +1,44 @@
+import { auth } from '@googleapis/docs';
+import { Router } from 'express';
+import { google } from 'googleapis';
+import * as dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+
+const googleAuthRouter = Router();
+
+const oauth2Client = new auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.REDIRECT_URL
+);
+
+// generate a url that asks permissions for Blogger and Google Calendar scopes
+
+googleAuthRouter.get('/auth/google/url', (req, res) => {
+  const scopes = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'];
+  const url = oauth2Client.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: 'offline',
+
+    // If you only need one scope you can pass it as a string
+    scope: scopes,
+  });
+  return res.send(url);
+});
+
+googleAuthRouter.get('/auth/google', async (req, res, next) => {
+  try {
+    const oauth2 = google.oauth2({ auth: oauth2Client, version: 'v2' });
+    const { access_token, id_token } = (await oauth2Client.getToken(req.query.code as string)).tokens;
+    oauth2Client.setCredentials({ access_token, id_token });
+    const user = (await oauth2.userinfo.get()).data;
+    res.cookie('JWT', jwt.sign(user, process.env.JWT_SECRET));
+    res.redirect('/hello');
+  } catch (e) {
+    next(e);
+  }
+});
+
+export default googleAuthRouter;
