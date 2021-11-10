ef54c5b41be3f75a3ab968d0673d95409097cadb
Tahabz <etahabaz@gmail.com>
2021-11-10 13:29:24 +0100

    add disconnect method to index.ts

---
+++ src/db/index.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+dotenv.config();
+let DB_URL = process.env.DB_CONNECTION;
+export const connect = async () => {
+  if (process.env.NODE_ENV == 'test') {
+    const mongod = await MongoMemoryServer.create();
+    DB_URL = mongod.getUri();
+  }
+  return mongoose
+    .connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as mongoose.ConnectOptions)
+    .then(() => console.log('Mongo successfully connected'))
+    .catch((e) => console.error(e));
+};
+
+export const disconnect = () => {
+  return mongoose.disconnect();
+};
