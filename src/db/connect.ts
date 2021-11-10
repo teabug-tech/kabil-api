78f99513d9167b8037774c0bea9bf0b965c5c580
Tahabz <etahabaz@gmail.com>
2021-11-10 11:22:13 +0100

    use the in memory mongodb db in case of NODE_ENV=test

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,12 +1,19 @@
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 dotenv.config();
-export default () =>
-  mongoose
-    .connect(process.env.DB_CONNECTION, {
+let DB_URL = process.env.DB_CONNECTION;
+export default async () => {
+  if (process.env.NODE_ENV == 'test') {
+    const mongod = await MongoMemoryServer.create();
+    DB_URL = mongod.getUri();
+  }
+  return mongoose
+    .connect(DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     } as mongoose.ConnectOptions)
     .then(() => console.log('Mongo successfully connected'))
     .catch((e) => console.error(e));
+};
