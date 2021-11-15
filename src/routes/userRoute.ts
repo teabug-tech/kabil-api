9ff4e6c76469441fd92fc8fb7b0636805d46d5cd
Tahabz <etahabaz@gmail.com>
2021-11-15 14:43:10 +0100

    add more user routes

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -3,6 +3,18 @@ import UserController from '../controllers/UserController';
 
 const userRouter = express();
 
+userRouter.use(express.json());
+
+userRouter.get('/:dialect', async (req, res, next) => {
+  const select = UserController.getMany({ dialect: req.params.dialect });
+  const exec = select('firstname lastname score gender role');
+  return await exec(req, res, next);
+});
+
+userRouter.get('/', async (req, res, next) => {
+  return await UserController.getAll(req, res, next);
+});
+
 userRouter.get('/:id', async (req, res, next) => {
   const select = UserController.getOne({ _id: req.params.id });
   const exec = select('firstname lastname score gender role');
@@ -10,7 +22,16 @@ userRouter.get('/:id', async (req, res, next) => {
 });
 
 userRouter.post('/', async (req, res, next) => {
+  // return res.end('hello world');
   return await UserController.createOne(req, res, next);
+});
+
+userRouter.delete('/', async (req, res, next) => {
+  return await UserController.deleteOne(req, res, next);
+});
+
+userRouter.put('/', async (req, res, next) => {
+  return await UserController.updateOne(req, res, next);
 });
 
 export default userRouter;
