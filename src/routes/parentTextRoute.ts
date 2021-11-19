e7d6452243d9047ca326aacdf56c7d40b8d4eade
Tahabz <etahabaz@gmail.com>
2021-11-19 11:56:59 +0100

    add ParentTextRoute

---
+++ src/routes/parentTextRoute.ts
@@ -0,0 +1,15 @@
+import express, { Router } from 'express';
+import ParentTextController from '../controllers/ParentTextController';
+// import errorMiddleware from '../middlewares/errorMiddleware';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+let ParentTextRouter = Router();
+
+if (process.env.NODE_ENV == 'test') ParentTextRouter = express();
+
+ParentTextRouter.post('/', ParentTextController.createOne);
+ParentTextRouter.put('/', ParentTextController.updateOne);
+
+export default ParentTextRouter;
