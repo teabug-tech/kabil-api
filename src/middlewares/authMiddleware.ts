532c4933ed3dcf6c23b93c2c2d373f16ab55b561
Tahabz <etahabaz@gmail.com>
2021-11-27 09:14:05 +0100

    add user and admin auth middleware methods

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,16 +1,35 @@
 import jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
 import { NextFunction, Request, Response } from 'express';
+import UserService from '../services/UserService';
 
 dotenv.config();
 
-export default (req: Request, res: Response, next: NextFunction) => {
+export const adminAuth = (req: Request, res: Response, next: NextFunction) => {
   try {
-    let token = req.cookies['JWT'];
+    let token;
+    if (Object.prototype.hasOwnProperty.call(req, 'cookies')) token = req.cookies['JWT'];
     if (!token) {
       token = req.headers['authorization'].split(' ')[1];
     }
-    jwt.verify(token, process.env.JWT_SECRET);
+    const res = jwt.verify(token, process.env.JWT_SECRET);
+    if (res.role != 'admin') throw new Error('not allowed');
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const userAuth = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    let token;
+    if (Object.prototype.hasOwnProperty.call(req, 'cookies')) token = req.cookies['JWT'];
+    if (!token) {
+      token = req.headers['authorization'].split(' ')[1];
+    }
+    const res = jwt.verify(token, process.env.JWT_SECRET);
+    if (res.role != 'user') throw new Error('not allowed');
+    // req.user = admn;
     next();
   } catch (e) {
     next(e);
