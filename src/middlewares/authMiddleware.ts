dbf137cc59000652d18bf05e660858ccba2428ee
Tahabz <etahabaz@gmail.com>
2021-11-27 09:32:31 +0100

    add middleware admin/user auth methods

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -4,13 +4,28 @@ import { NextFunction, Request, Response } from 'express';
 
 dotenv.config();
 
-export default (req: Request, res: Response, next: NextFunction) => {
+export const adminAuth = (req: Request, res: Response, next: NextFunction) => {
   try {
     let token = req.cookies['JWT'];
     if (!token) {
       token = req.headers['authorization'].split(' ')[1];
     }
-    jwt.verify(token, process.env.JWT_SECRET);
+    const { role } = jwt.verify(token, process.env.JWT_SECRET);
+    if (role != 'admin') throw new Error('not allowed');
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const userAuth = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    let token = req.cookies['JWT'];
+    if (!token) {
+      token = req.headers['authorization'].split(' ')[1];
+    }
+    const { role } = jwt.verify(token, process.env.JWT_SECRET);
+    if (role != 'user') throw new Error('not allowed');
     next();
   } catch (e) {
     next(e);
