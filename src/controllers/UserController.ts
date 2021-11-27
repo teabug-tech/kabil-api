961cc9fc243b7477c76014573aa040ed853eb153
Tahabz <etahabaz@gmail.com>
2021-11-27 11:34:06 +0100

    override createOne in userController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,23 @@
+import { NextFunction, Response, Request } from 'express';
+import DialectService from '../services/DialectService';
 import UserService from '../services/UserService';
 import controller from '../shared/controller';
 
-export default controller(UserService);
+export default {
+  ...controller(UserService),
+  createOne: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body.data;
+      let dialect = await DialectService.getOne({ name: data.name })()();
+      if (!dialect) dialect = await DialectService.createOne({ name: data.dialect })();
+      data.dialect = dialect._id;
+      console.log(dialect);
+      const exec = UserService.createOne(data);
+      const user = await exec();
+      req.body.user = { _id: user._id, role: user.role };
+      next();
+    } catch (e) {
+      next(e);
+    }
+  },
+};
