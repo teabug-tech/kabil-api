37aa1c941e7c5d8116646a63390ccd090aaffebb
Tahabz <etahabaz@gmail.com>
2021-11-28 13:29:00 +0100

    add route to get random parentText

--- src/routes/parentTextRoute.ts
+++ src/routes/parentTextRoute.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import ParentTextController from '../controllers/ParentTextController';
 // import errorMiddleware from '../middlewares/errorMiddleware';
 import * as dotenv from 'dotenv';
+import parentTextModel from '../models/ParentText';
 
 dotenv.config();
 
@@ -11,5 +12,9 @@ if (process.env.NODE_ENV == 'test') ParentTextRouter = express();
 
 ParentTextRouter.post('/', ParentTextController.createOne);
 ParentTextRouter.put('/', ParentTextController.updateOne);
+ParentTextRouter.get('/', async (req, res) => {
+  const text = await parentTextModel.aggregate().match({ isCompleted: false }).sample(1).exec();
+  res.send(text);
+});
 
 export default ParentTextRouter;
