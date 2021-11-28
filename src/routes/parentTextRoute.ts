cc66a14046022e44f8aae9d876b2226fad0a9e00
Tahabz <etahabaz@gmail.com>
2021-11-28 15:20:55 +0100

    return random parentText when all are completed

--- src/routes/parentTextRoute.ts
+++ src/routes/parentTextRoute.ts
@@ -13,7 +13,8 @@ if (process.env.NODE_ENV == 'test') ParentTextRouter = express();
 ParentTextRouter.post('/', ParentTextController.createOne);
 ParentTextRouter.put('/', ParentTextController.updateOne);
 ParentTextRouter.get('/', async (req, res) => {
-  const text = await parentTextModel.aggregate().match({ isCompleted: false }).sample(1).exec();
+  let text = await parentTextModel.aggregate().match({ isCompleted: false }).sample(1).exec();
+  if (!text.length) text = await parentTextModel.aggregate().sample(1).exec();
   res.send(text);
 });
 
