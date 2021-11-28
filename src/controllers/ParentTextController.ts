301c9afa38bc8e6a84a53b9f696f087c60f09862
Tahabz <etahabaz@gmail.com>
2021-11-28 15:26:37 +0100

    override the parentTextController getOne method

--- src/controllers/ParentTextController.ts
+++ src/controllers/ParentTextController.ts
@@ -1,4 +1,13 @@
+import { Request, Response } from 'express';
+import parentTextModel from '../models/ParentText';
 import ParentTextService from '../services/ParentTextService';
 import controller from '../shared/controller';
 
-export default controller(ParentTextService);
+export default {
+  ...controller(ParentTextService),
+  getOne: async (req: Request, res: Response) => {
+    let text = await parentTextModel.aggregate().match({ isCompleted: false }).sample(1).exec();
+    if (!text.length) text = await parentTextModel.aggregate().sample(1).exec();
+    res.send(text);
+  },
+};
