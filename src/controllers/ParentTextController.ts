64dd32f7c7a798526b3c23d89301e0d9b5580d91
Tahabz <etahabaz@gmail.com>
2021-12-03 15:23:19 +0100

    add error handling in parentText Controller

--- src/controllers/ParentTextController.ts
+++ src/controllers/ParentTextController.ts
@@ -1,40 +1,37 @@
-import { Request, Response, text } from 'express';
-import { IDomain } from '../models/Domain';
+import { NextFunction, Response } from 'express';
+import { Types } from 'mongoose';
 import parentTextModel, { IParentText } from '../models/ParentText';
 import { IScript } from '../models/Scripts';
 import { IVoice } from '../models/Voice';
 import ArabicScriptService from '../services/ArabicScriptService';
-import DomainService from '../services/DomainService';
 import LatinScriptService from '../services/LatinScriptService';
 import ParentTextService from '../services/ParentTextService';
 import VoiceService from '../services/VoiceService';
 import controller from '../shared/controller';
 import { IRequest } from '../types';
 
+type Ops = keyof typeof services;
+type ParentRefs = IScript | IVoice;
+
 const services = {
   latinScript: async (data) => await LatinScriptService.createOne({ user: data.user, script: data.latinScript })(),
   arabicScript: async (data) => await ArabicScriptService.createOne({ user: data.user, script: data.arabicScript })(),
   voice: async (data) => await VoiceService.createOne({ user: data.user, url: data.voice })(),
-  domain: async (data) => await DomainService.createOne({ user: data.user, name: data.bname })(),
 };
 
-type Ops = keyof typeof services;
-type Data = IScript | IVoice | IDomain;
-
-const applyOp = (op: Ops) => async (data: IScript | IVoice | IDomain) => {
+const applyOp = (op: Ops) => async (data: ParentRefs) => {
   const res = await services[op](data);
   return res._id;
 };
 
-const makeParentObject = async (body: IParentText, user) => {
+const makeParentObject = async (body: IParentText, user: Types.ObjectId) => {
   const parent = { ...body };
   for (const key in body) {
     if (key in services) {
       const getId = applyOp(key as Ops);
-      parent[key] = await getId({ user: user._id, [key]: body[key] } as Data);
+      parent[key] = await getId({ user, [key]: body[key] } as ParentRefs);
     }
   }
-  console.log(parent);
   return parent;
 };
 
@@ -46,11 +43,15 @@ export default {
     if (!text.length) text = await parentTextModel.aggregate().sample(1).exec();
     res.send(text);
   },
-  createOne: async (req: IRequest, res: Response) => {
-    const body = req.body.data;
-    const parent: IParentText = await makeParentObject(body, req.user);
-    const exec = ParentTextService.createOne(parent);
-    const result = await exec();
-    res.send(result);
+  createOne: async (req: IRequest, res: Response, next: NextFunction) => {
+    try {
+      const body = req.body.data;
+      const parent: IParentText = await makeParentObject(body, req.user._id);
+      const exec = ParentTextService.createOne(parent);
+      const result = await exec();
+      res.send(result);
+    } catch (e) {
+      next(e);
+    }
   },
 };
