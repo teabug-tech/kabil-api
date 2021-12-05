e76807d58afa7341ff5e60e680501746ea63b9f9
Tahabz <etahabaz@gmail.com>
2021-12-05 12:38:17 +0100

    add IChildData type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,18 @@
 import { Request } from 'express';
 import { IUser } from './models/User';
+import { Types } from 'mongoose';
 
 export interface IRequest extends Request {
   user: IUser;
+}
+
+export interface IChildData {
+  _id?: Types.ObjectId;
+  arabicScript?: Types.ObjectId;
+  latinScript?: Types.ObjectId;
+  voice?: Types.ObjectId;
+  domain?: Types.ObjectId;
+  dialect?: Types.ObjectId;
+  gender?: 'male' | 'female';
+  parent?: Types.ObjectId;
 }
