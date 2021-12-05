feccbb25ed01c9be1cc69e06be6e6ce21cf213d9
Tahabz <etahabaz@gmail.com>
2021-12-05 12:41:53 +0100

    add get '/' route to childTextRoute

--- src/routes/childTextRoute.ts
+++ src/routes/childTextRoute.ts
@@ -4,6 +4,7 @@ import ChildTextController from '../controllers/ChildTextController';
 const childTextRouter = Router();
 
 childTextRouter.get(':/id', ChildTextController.getOne);
+childTextRouter.get('/', ChildTextController.getAll);
 childTextRouter.route('/').post(ChildTextController.createOne).delete(ChildTextController.deleteOne);
 
 export default childTextRouter;
