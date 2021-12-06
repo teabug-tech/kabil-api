89dde0e054f5999191f7dfff018a3d5012519bbe
Tahabz <etahabaz@gmail.com>
2021-12-06 21:32:43 +0100

    get populated parent texts

--- src/routes/parentTextRoute.ts
+++ src/routes/parentTextRoute.ts
@@ -2,6 +2,8 @@ import express, { Router } from 'express';
 import ParentTextController from '../controllers/ParentTextController';
 // import errorMiddleware from '../middlewares/errorMiddleware';
 import * as dotenv from 'dotenv';
+import { IRequest } from '../types';
+import { PopulateOptions } from 'mongoose';
 
 dotenv.config();
 
@@ -11,6 +13,12 @@ if (process.env.NODE_ENV == 'test') ParentTextRouter = express();
 
 ParentTextRouter.post('/', ParentTextController.createOne);
 ParentTextRouter.put('/', ParentTextController.updateOne);
-ParentTextRouter.get('/', ParentTextController.getOne);
+ParentTextRouter.get('/:id', async (req: IRequest, res, next) => {
+  return await ParentTextController.getOneAndPopulate({ _id: req.params.id })([
+    'arabicScript',
+    'latinScript',
+    'dialect',
+  ])()(req, res, next);
+});
 
 export default ParentTextRouter;
