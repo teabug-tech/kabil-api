f57149cad006b5545e19796f5a2977c02455b8c8
Tahabz <etahabaz@gmail.com>
2021-12-07 11:52:47 +0100

    add populate when aggregatings a parenttext

--- src/controllers/ParentTextController.ts
+++ src/controllers/ParentTextController.ts
@@ -1,10 +1,7 @@
 import { NextFunction, Response } from 'express';
 import { Types } from 'mongoose';
-import { IDialect } from '../models/Dialect';
-import { IDomain } from '../models/Domain';
-import parentTextModel, { IParentText } from '../models/ParentText';
-import { IScript } from '../models/Scripts';
-import { IVoice } from '../models/Voice';
+import childTextModel from '../models/ChildText';
+import parentTextModel from '../models/ParentText';
 import ArabicScriptService from '../services/ArabicScriptService';
 import DialectService from '../services/DialectService';
 import DomainService from '../services/DomainService';
@@ -12,10 +9,9 @@ import LatinScriptService from '../services/LatinScriptService';
 import ParentTextService from '../services/ParentTextService';
 import VoiceService from '../services/VoiceService';
 import controller from '../shared/controller';
-import { IRequest } from '../types';
+import { IParentText, IRequest, Refs } from '../types';
 
-type Ops = keyof typeof services;
-type ParentRefs = IScript | IVoice | IDomain | IDialect;
+export type Ops = keyof typeof services;
 
 const services = {
   latinScript: async (data) => await LatinScriptService.createOne({ user: data.user, script: data.latinScript })(),
@@ -25,23 +21,23 @@ const services = {
   dialect: async (data) => await DialectService.createOne({ name: data.dialect })(),
 };
 
-const applyOp = (op: Ops) => async (data: ParentRefs) => {
+const applyOp = (op: Ops) => async (data: Refs) => {
   const res = await services[op](data);
   return res._id;
 };
 
-const makeParentObject = async (data: IParentText, user: Types.ObjectId) => {
+const makeParentObject = async (data: Refs, user: Types.ObjectId) => {
   const parent = { ...data };
   for (const key in data) {
     if (key in services) {
       const getId = applyOp(key as Ops);
-      parent[key] = await getId({ user, [key]: data[key] } as ParentRefs);
+      parent[key] = await getId({ user, [key]: data[key] } as Refs);
     }
   }
   return parent;
 };
 
-const validateParentData = async (data: IParentText, id: Types.ObjectId) => {
+const validateParentData = async (data: Refs, id: Types.ObjectId) => {
   const parentData = { ...data };
   const parent = await ParentTextService.getOne({ _id: id })()();
   for (const key in parentData) {
@@ -51,12 +47,36 @@ const validateParentData = async (data: IParentText, id: Types.ObjectId) => {
   }
 };
 
+interface Lookup {
+  $lookup: {
+    from: string;
+    localField: string;
+    foreignField: string;
+    as: string;
+  };
+}
+
+const makeLookupObjects = (lookups: Array<string>): Array<Lookup> =>
+  lookups.map((l) => ({
+    $lookup: {
+      from: l.toLowerCase() + 's',
+      localField: l,
+      foreignField: '_id',
+      as: l,
+    },
+  }));
+
 export default {
   ...controller(ParentTextService),
   getOne: async (req: IRequest, res: Response) => {
     console.log(req.user);
-    let text = await parentTextModel.aggregate().match({ isCompleted: false }).sample(1).exec();
-    if (!text.length) text = await parentTextModel.aggregate().sample(1).exec();
+    const lookupObjects = makeLookupObjects(['arabicScript', 'latinScript', 'voice', 'domain', 'voice', 'dialect']);
+    let text = await parentTextModel
+      .aggregate([...lookupObjects])
+      .match({ isCompleted: false })
+      .sample(1)
+      .exec();
+    if (!text.length) text = await childTextModel.aggregate().sample(1).exec();
     res.send(text);
   },
   createOne: async (req: IRequest, res: Response, next: NextFunction) => {
