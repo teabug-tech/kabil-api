dc2a6598065cfcc44ba64116fbb2a7771e45ac12
Tahabz <etahabaz@gmail.com>
2021-12-07 17:23:16 +0100

    hash password before save

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import { model, Schema } from 'mongoose';
 import { Refvalidator } from '../shared/existValidator';
 import { IUser } from '../types';
 import dialectModel from './Dialect';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema<IUser>({
   email: {
@@ -47,6 +48,20 @@ const userSchema = new Schema<IUser>({
 });
 
 userSchema.path('dialect').validate(Refvalidator(dialectModel), 'invalid references');
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.hash(this.password, 10, (err, passwordHash) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = passwordHash;
+    return next();
+  });
+});
 
 const userModel = model<IUser>('user', userSchema);
 
