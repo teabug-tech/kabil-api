bf47a0e998c08cd73a98f5b608165ca7d2ba9b4c
Tahabz <etahabaz@gmail.com>
2021-12-07 15:26:52 +0100

    check users authorization before accessing to user routes

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -2,7 +2,9 @@ import express, { Router } from 'express';
 import UserController from '../controllers/UserController';
 import errorMiddleware from '../middlewares/errorMiddleware';
 import * as dotenv from 'dotenv';
-import AuthController from '../controllers/AuthController';
+import AuthController, { login } from '../controllers/AuthController';
+import { adminAuth, userAuth } from '../middlewares/authMiddleware';
+import { IRequest } from '../types';
 
 dotenv.config();
 
@@ -13,23 +15,36 @@ if (process.env.NODE_ENV == 'test') {
   userRouter.use(express.json());
 }
 
-userRouter.get('/', async (req, res, next) => {
+userRouter.post('/login', login);
+userRouter.post('/', UserController.createOne);
+
+userRouter.delete('/', adminAuth, UserController.deleteOne);
+
+userRouter.get('/', adminAuth, async (req: IRequest, res, next) => {
   const select = UserController.getMany({ ...req.query });
   const exec = select('firstName lastName score');
   return await exec(req, res, next);
 });
 
-userRouter.get('/:id', async (req, res, next) => {
-  const select = UserController.getOne({ _id: req.params.id });
-  const exec = select('firstName lastName score');
-  return await exec(req, res, next);
+userRouter.get('/:id', userAuth, async (req: IRequest, res, next) => {
+  try {
+    if (req.user._id.toString() !== req.params.id) throw new Error('Invalid request');
+    const select = UserController.getOne({ _id: req.params.id });
+    const exec = select('firstName lastName score');
+    return await exec(req, res, next);
+  } catch (e) {
+    next(e);
+  }
 });
 
-userRouter.post('/', UserController.createOne);
-
-userRouter.delete('/', UserController.deleteOne);
-
-userRouter.put('/', UserController.updateOne);
+userRouter.put('/', async (req: IRequest, res, next) => {
+  try {
+    if (req.user._id.toString() !== req.params.id) throw new Error('Invalid request');
+    return await UserController.updateOne(req, res, next);
+  } catch (e) {
+    next(e);
+  }
+});
 
 if (process.env.NODE_ENV == 'test') userRouter.use(errorMiddleware);
 
