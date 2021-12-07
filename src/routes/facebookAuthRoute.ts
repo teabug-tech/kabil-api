4d6cd2e756141e17071c81cbb019fdfd3b4474d1
Tahabz <etahabaz@gmail.com>
2021-12-07 15:28:27 +0100

    fix small bugs in google and facebook auth

--- src/routes/facebookAuthRoute.ts
+++ src/routes/facebookAuthRoute.ts
@@ -1,22 +1,21 @@
 import { Router } from 'express';
-import * as queryString from 'query-string';
+// import * as queryString from 'query-string';
 import * as dotenv from 'dotenv';
 import axios from 'axios';
-import jwt from 'jsonwebtoken';
 
 dotenv.config();
 const facebookAuthRouter = Router();
 
-const stringifiedParams = queryString.stringify({
-  client_id: process.env.FACEBOOK_CLIENT_ID,
-  redirect_uri: process.env.FACEBOOK_REDIRECT_URL,
-  scope: ['public_profile', ' email'].join(','), // comma seperated string
-  response_type: 'code',
-  auth_type: 'rerequest',
-  display: 'popup',
-});
+// const stringifiedParams = queryString.stringify({
+//   client_id: process.env.FACEBOOK_CLIENT_ID,
+//   redirect_uri: process.env.FACEBOOK_REDIRECT_URL,
+//   scope: ['public_profile', ' email'].join(','), // comma seperated string
+//   response_type: 'code',
+//   auth_type: 'rerequest',
+//   display: 'popup',
+// });
 
-const facebookLoginUrl = `https://www.facebook.com/v4.0/dialog/oauth?${stringifiedParams}`;
+// const facebookLoginUrl = `https://www.facebook.com/v4.0/dialog/oauth?${stringifiedParams}`;
 
 async function getAccessTokenFromCode(code) {
   const { data } = await axios({
@@ -44,20 +43,19 @@ async function getFacebookUserData(access_token) {
   return data;
 }
 
-facebookAuthRouter.get('/auth/facebook/url', (req, res) => {
+facebookAuthRouter.get('/facebook/url', (req, res) => {
   const facebookUrl = `https://www.facebook.com/v12.0/dialog/oauth?
                     client_id=${process.env.FACEBOOK_CLIENT_ID}
                     &redirect_uri=${process.env.FACEBOOK_REDIRECT_URL}`;
   return res.end(facebookUrl);
 });
 
-facebookAuthRouter.get('/auth/facebook', async (req, res, next) => {
+facebookAuthRouter.get('/facebook', async (req, res, next) => {
   try {
     const code = req.query.code;
     const token = await getAccessTokenFromCode(code);
     const user = await getFacebookUserData(token);
-    res.cookie('JWT', jwt.sign(user, process.env.JWT_SECRET));
-    res.redirect('/hello');
+    return res.send(user);
   } catch (e) {
     next(e);
   }
--- src/routes/googleAuthRoute.ts
+++ src/routes/googleAuthRoute.ts
@@ -2,7 +2,6 @@ import { auth } from '@googleapis/docs';
 import { Router } from 'express';
 import { google } from 'googleapis';
 import * as dotenv from 'dotenv';
-import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
@@ -11,12 +10,12 @@ const googleAuthRouter = Router();
 const oauth2Client = new auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URL
+  process.env.GOOGLE_REDIRECT_URL,
 );
 
 // generate a url that asks permissions for Blogger and Google Calendar scopes
 
-googleAuthRouter.get('/auth/google/url', (req, res) => {
+googleAuthRouter.get('/google/url', (req, res) => {
   const scopes = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'];
   const url = oauth2Client.generateAuthUrl({
     // 'online' (default) or 'offline' (gets refresh_token)
@@ -28,14 +27,13 @@ googleAuthRouter.get('/auth/google/url', (req, res) => {
   return res.send(url);
 });
 
-googleAuthRouter.get('/auth/google', async (req, res, next) => {
+googleAuthRouter.get('/google', async (req, res, next) => {
   try {
     const oauth2 = google.oauth2({ auth: oauth2Client, version: 'v2' });
     const { access_token, id_token } = (await oauth2Client.getToken(req.query.code as string)).tokens;
     oauth2Client.setCredentials({ access_token, id_token });
     const user = (await oauth2.userinfo.get()).data;
-    res.cookie('JWT', jwt.sign(user, process.env.JWT_SECRET));
-    res.redirect('/hello');
+    return res.send(user);
   } catch (e) {
     next(e);
   }
