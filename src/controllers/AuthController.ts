05d251292df5a78ffb37545a153d5c2193869578
Tahabz <etahabaz@gmail.com>
2021-12-07 15:26:35 +0100

    add login auth controller method

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
 import { NextFunction, Request, Response } from 'express';
+import UserService from '../services/UserService';
 
 dotenv.config();
 export default (req: Request, res: Response, next: NextFunction) => {
@@ -8,6 +9,19 @@ export default (req: Request, res: Response, next: NextFunction) => {
     const token = jwt.sign({ ...req.body.user }, process.env.JWT_SECRET);
     res.cookie('JWT', token);
     return res.json({ success: true, message: req.body.user });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const user = req.body.user;
+
+    const userDoc = await UserService.getOne({ email: user.email })()();
+    if (!userDoc) throw new Error('invalid credentials!');
+    if (userDoc.password !== user.password) throw new Error('invalid credentials');
+    return res.json({ success: true, message: userDoc });
   } catch (e) {
     next(e);
   }
