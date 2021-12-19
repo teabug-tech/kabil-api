028a4e80e244bd3101b74d87d709cc1d030959bb
Tahabz <etahabaz@gmail.com>
2021-12-19 12:12:44 +0100

    add allowedHeaders to cors options

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,13 @@ import cors from 'cors';
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  }),
+);
 
 app.use(morgan('dev'));
 app.use(express.json());
