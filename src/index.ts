dd48697e9caf514cbbb2fb684521af71baa2f396
Tahabz <etahabaz@gmail.com>
2021-12-19 12:18:53 +0100

    add allowedHeaders to cors options

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: 'http://localhost:3000',
     credentials: true,
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+    allowedHeaders: '*',
   }),
 );
 
