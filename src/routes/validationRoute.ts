14ca2df2678fbef581bfa72a1078a7dd0ee18b97
Tahabz <etahabaz@gmail.com>
2022-02-01 22:21:12 +0100

    check if text exists before adding validation

--- src/routes/validationRoute.ts
+++ src/routes/validationRoute.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import ValidationController from '../controllers/ValidationController';
+import childTextModel from '../models/ChildText';
 import validationModel from '../models/Validation';
 
 const validationRouter = Router();
@@ -10,6 +11,8 @@ validationRouter.get('/', (req, res) => {
 validationRouter.post('/', async (req, res, next) => {
   try {
     const data = req.body.data;
+    if (!(await childTextModel.findById(data.text)))
+      res.status(404).json({ success: false, message: 'text does not exist' });
     const doc = await validationModel.findOneAndUpdate(
       { text: data.text },
       { $push: { answers: data.answer } },
