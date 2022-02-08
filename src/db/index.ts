fd1296fe86934c3145088d808a2ee7a228a77057
JLL32 <eabduljalil@outlook.com>
2022-02-08 13:52:40 +0100

    disable mongo-memory

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
-import { MongoMemoryServer } from 'mongodb-memory-server';
+// import { MongoMemoryServer } from 'mongodb-memory-server';
 
 dotenv.config();
-let DB_URL = process.env.DB_CONNECTION;
+const DB_URL = process.env.DB_CONNECTION;
 export const connect = async () => {
-  if (process.env.NODE_ENV == 'test') {
-    console.log('TESTING MODE');
-    const mongod = await MongoMemoryServer.create();
-    DB_URL = mongod.getUri();
-  }
+  // if (process.env.NODE_ENV == 'test') {
+  //   console.log('TESTING MODE');
+  //   const mongod = await MongoMemoryServer.create();
+  //   DB_URL = mongod.getUri();
+  // }
   return mongoose
     .connect(DB_URL, {
       useNewUrlParser: true,
